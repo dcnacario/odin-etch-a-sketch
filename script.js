cb224b6be4d4020f340dc8747a36b7fb0e17b578
Danmar Cornelio Nacario <dcnacario08@gmail.com>
2023-09-21 18:59:12 +0800

    ADD COLOR TO THE PIXELS
    creating event for adding color by hovering to the boxes.

--- script.js
+++ script.js
@@ -7,26 +7,64 @@ btnAsk.appendChild(btnAskText);
 setting.appendChild(btnAsk);
 
 
+
+for(let i = 1 ; i < 272  ; i++){
+    const div = document.createElement("div");
+    div.className = "child";
+    if(i % (16+1) === 0){
+        div.style.cssText = "border: 0; height: 0; width: 100%";
+    }
+    else {
+        div.style.cssText = "border: 0.5px solid #CCC8AA; aspect-ratio: 1/1; flex:1";
+    }
+    container.appendChild(div);
+}
+
 const gridPrompt = btnAsk.addEventListener("click", ()=>{
-    let numberPerRow = Number(prompt("How many rows?"));
-    const total = (numberPerRow * numberPerRow) + numberPerRow;
-    const mod = numberPerRow + 1;
-    let child = container.firstElementChild;
-    while(child) {
-        container.removeChild(child);
-        child = container.firstElementChild;
+    let numberPerRow = Number(prompt("How many rows(1-100)?"));
+    if(numberPerRow > 100 || numberPerRow < 1){
+         numberPerRow = Number(prompt("Please input(1-100)!"));
     }
-    for(let i = 1 ; i < total  ; i++){
-        const div = document.createElement("div");
-        if(i % mod === 0){
-            div.style.cssText = "border: 0; height: 0; width: 100%";
+    else{
+        const total = (numberPerRow * numberPerRow) + numberPerRow;
+        const mod = numberPerRow + 1;
+        let child = container.firstElementChild;
+        while(child) {
+            container.removeChild(child);
+            child = container.firstElementChild;
         }
-        else {
-            div.style.cssText = "border: 1px solid black; aspect-ratio: 1/1; flex:1";
+        for(let i = 1 ; i < total  ; i++){
+            const div = document.createElement("div");
+            div.className = "child";
+            if(i % mod === 0){
+                div.style.cssText = "border: 0; height: 0; width: 100%";
+            }
+            else {
+                div.style.cssText = "border: 0.5px solid #CCC8AA; aspect-ratio: 1/1; flex:1";
+            }
+            container.appendChild(div);
         }
-        container.appendChild(div);
     }
-    
 });
+
+let parentContainer = document.getElementById("container");
+let childContainer = parentContainer.children
+console.log(childContainer);
+
+parentContainer.addEventListener("mouseover", (event)=>{
+    const childElement = event.target;
+    
+    for (let j = 0; j < childContainer.length; j++) {
+        if (childElement === childContainer[j]) {
+          childContainer[j].style.backgroundColor = '#7D7C7C';
+        }
+    }
+});
+
+
+
+
+
+
 
 
